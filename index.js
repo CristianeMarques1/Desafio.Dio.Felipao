33b6bc182b51a3879da2352ff68751b52970f4e5
CristianeMarques1 <151093954+CristianeMarques1@users.noreply.github.com>
2023-12-01 01:01:19 +0000

    Finaly.  Git add

---
+++ index.js
@@ -0,0 +1,9 @@
+let nomeDeHeroina = ("Wanda")
+let pontoDeExperiencia = ( 9683)
+
+if (pontoDeExperiencia <= 2000) {
+    print ( "prata")
+}
+else if  ( pontoDeExperiencia >= 9000  ){ 
+    print ( " Esta heroina de nome:" + nomeDeHeroina + "Esta no nivel:ouro")
+}
